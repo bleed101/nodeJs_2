a318eb88145b3d9d62625e4f40e2a6261ba5760e
bleed101 <entender8ahora@gmail.com>
2024-01-27 03:20:38 +0530

    sec02|12-23:web server, templating, package versioning, etc

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
 const http = require("http");
+const path = require("path");
 
+const slugify = require("slugify");
+
+const replaceTemplate = require("./modules/replaceTemplate");
 // File handling.
 /*
 // Blocking, synchronous way.
@@ -26,13 +30,54 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
 });
 */
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  // path.join(__dirname, "templates", "template-overview.html"),
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  path.join(__dirname, "templates", "template-product.html"),
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  // path.join(__dirname, "templates", "template-card.html"),
+  "utf-8"
+);
+const apiData = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const apiDataObj = JSON.parse(apiData);
+
+const slugs = apiDataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+
+console.log(`${__dirname}\\templates\\template-card.html`);
 //Server creation.
 const server = http.createServer((req, res) => {
+  // console.log(req.url);
   const prefix = req.url;
+  // Overview page
   if (prefix === "/" || prefix === "/overview") {
-    res.end("This is overview");
-  } else if (prefix === "/product") {
-    res.end("This is product page.");
+    res.writeHead(200, { "content-type": "text/html" });
+    const cardsHtml = apiDataObj.map((el) => replaceTemplate(tempCard, el));
+    console.log(cardsHtml);
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+
+    // Product
+  } else if (prefix === `/product?id=${prefix.split("=")[1]}`) {
+    res.writeHead(200, { "content-type": "text/html" });
+    const cardsHtml = apiDataObj.map((el) => replaceTemplate(tempProduct, el));
+    // const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml[prefix.split("=")[1]]);
+    res.end(cardsHtml[prefix.split("=")[1]]);
+
+    // API
+  } else if (prefix === "/api") {
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+    res.end(apiData);
+
+    // Not found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
